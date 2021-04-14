6de2fabf4e44ac46d9e69897e9931c57862e38dc
jjan3640 <jjan3640@uni.sydney.edu.au>
2021-04-14 13:53:16 +1000

    created the backend foundation and linked it to the frontend

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,13 +1,23 @@
+import { useState, useEffect } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          COMP5347 Assignment 2
+          {data ? data : "Is the backend running?"}
         </p>
         <a
           className="App-link"
