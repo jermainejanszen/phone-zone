f22fde30a071171865e69a9b6a8d236822b5ba1c
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-04-17 17:48:39 +1000

    made an animated loading component

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,46 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import NavBar from '../components/NavBar';
-import logo from '../resources/logo.svg';
+import Loading from '../components/Loading';
 import '../styles/App.css';
 
 const App = () => {
 
-  // 0 = home, 1 = search, 2 = item
-  const [pageState, setPageState] = useState(0);
+    // 0 = home, 1 = search, 2 = item
+    const [pageState, setPageState] = useState(0);
 
-  // 0 = signed out, 1 = signed in
-  const [authState, setAuthState] = useState(0);
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+    // 0 = signed out, 1 = signed in
+    const [authState, setAuthState] = useState(0);
 
   return (
     <div className="App">
-      <NavBar 
-        className="nav"
-        pageState={pageState} 
-        setPageState={setPageState} 
-        authState={authState}
-        setAuthState={setAuthState} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {data ? data : "Is the backend running?"}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NavBar 
+            className="nav"
+            pageState={pageState} 
+            setPageState={setPageState} 
+            authState={authState}
+            setAuthState={setAuthState} />
+        <Loading />
     </div>
   );
 }
