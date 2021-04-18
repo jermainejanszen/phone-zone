c9ce54781c7b02d077c43576edb862c1e9138905
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-04-18 14:01:14 +1000

    centered the lists on the home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,17 @@
 import FiveGrid from './FiveGrid';
 
+import '../styles/Home.css';
+
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <div className="topFiveGrid" id="sold-out-soon">
                 <h2>Sold Out Soon</h2>
-                <FiveGrid />
+                <FiveGrid className="grid" />
             </div>
             <div className="topFiveGrid" id="best-sellers">
                 <h2>Best Sellers</h2>
-                <FiveGrid />
+                <FiveGrid className="grid" />
             </div>
         </div>
     )
