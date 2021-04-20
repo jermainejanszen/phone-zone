ec0f16a864b8e3287fbc4cce13df0d1302819a28
hollycraig <holly.g.craig@gmail.com>
2021-04-20 14:01:59 +1000

    new profile component, updated profile state logic

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,4 +1,21 @@
 import { useState } from 'react';
+import Profile from '../components/Profile';
+import EditUser from '../components/EditUser';
+import EditPassword from '../components/EditPassword';
+import NewListing from '../components/NewListing';
+
+
+const getPageContents = (state) => {
+    if(state === 1) {
+        return <EditUser />;
+    } else if(state === 2) {
+        return <EditPassword />;
+    } else if (state === 3) {
+        return <NewListing />;
+    } else {
+        return <Profile />;
+    }
+}
 
 const User = () => {
 
