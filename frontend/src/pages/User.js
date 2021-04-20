e30e6b9ec25a9a93620fe35faa45dbf720fefcb5
hollycraig <holly.g.craig@gmail.com>
2021-04-20 13:52:23 +1000

    added states to user.js

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from 'react';
 
 const User = () => {
+
+    const [pageState, setPageState] = useState(0);
+
     return (
         <div>
             
