afaba909e754f91cafbea54b960d418643ebb659
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-04-21 23:20:50 +1000

    fixed styling for signup page

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-// import '../styles/Signup.css';
+import '../styles/Login.css';
 
 const Signup = () => {
+
+    const history = useHistory();
+
     return (
         <div className="form-container">
-            <form className="signup-form">
-                <h1>Create your account</h1>
+            <form className="login-form">
+                <h1 className="formHeading">Create your account</h1>
 
-                <label class="required" for="first-name">First Name</label>
-                <input title="first-name" type="text" required></input>
+                <div className="fieldDiv">
+                    <label className="formLabel required" for="first-name">First Name</label>
+                    <input className="formInputText" title="first-name" type="text" required />
+                </div>
 
-                <label class="required" for="last-name">Last Name</label>
-                <input title="last-name" type="text" required></input>
+                <div className="fieldDiv">
+                    <label className="formLabel required" for="last-name">Last Name</label>
+                    <input className="formInputText" title="last-name" type="text" required />
+                </div>
 
-                <label class="required" for="email">Email</label>
-                <input title="email" type="email" required></input>
+                <div className="fieldDiv">
+                    <label className="formLabel required" for="email">Email</label>
+                    <input className="formInputText" title="email" type="email" required />
+                </div>
 
-                <label class="required" for="password">Password</label>
-                <p class="advice">
-                    Make sure it's at least 8 characters including a lowercase and uppercase letter.
-                </p>
-                <input id="password" title="password" type="password" pattern="^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$" required></input>
+                <div className="fieldDiv">
+                    <div id="password-field-label">
+                        <label className="formLabel required" for="password">Password</label>
+                        <p class="advice">
+                            Make sure it's at least 8 characters including a lowercase and an uppercase letter.
+                        </p>
+                    </div>
+                    <input className="formInputText" id="password" title="password" type="password" pattern="^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$" required />
+                </div>
 
                 <div id="password-visibility-container">
                     <label id="password-visibility-label" for="password-visibility">Toggle Password Visibility</label>
@@ -38,11 +51,18 @@ const Signup = () => {
                     </input>
                 </div>
 
-                <input id="signup-button" type="submit" value="Create account"></input>
+                <div className="loginButtonsDiv">
+                    <input className="loginButtons" id="signup-button" type="submit" value="Create account" />
 
-                <Link to="/login">
-                    <input id="nav-button" type="button" value="Click here to login"></input>
-                </Link>
+                    <button 
+                        className="loginButtons" 
+                        id="nav-button"
+                        onClick={() => {
+                            history.push('/login');
+                        }}>
+                            Already have an account? Login here
+                    </button>
+                </div>
             </form>
         </div>
     );
