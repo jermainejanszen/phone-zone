cb1c4b762a313139b276627d85cfeec991834544
Ben Gane <bgan7208@uni.sydney.edu.au>
2021-04-21 16:22:37 +1000

    implemented basic frontend for the login page and navigation between the signup and login pages

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,35 +1,90 @@
 import React from 'react'
+import { useState } from 'react'
 
 import '../styles/Signup.css';
 
+
 const Signup = () => {
-    return (
-        <div id="container">
-            <form id="signup-form">
-                <h1>Create your account</h1>
-                <label class="required" for="first-name">First Name</label>
-                <input title="first-name" type="text" required></input>
-                <label class="required" for="last-name">Last Name</label>
-                <input title="last-name" type="text" required></input>
-                <label class="required" for="email">Email</label>
-                <input title="email" type="email" required></input>
-                <label class="required" for="password-field">Password</label>
-                <input id="password-text" title="password" type="password" pattern="^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$" required></input>
-                <div id="password-visibility-container">
-                    <label id="password-visibility-label" for="password-visibility">Toggle Password Visibility</label>
+
+    const [state, setState] = useState(0)
+
+    const getPageContents = () => {
+        if (state == 1) {
+            return (
+                <form id="signup-form">
+                    <h1>Login</h1>
+                    <label for="email">Email</label>
+                    <input title="email" type="email" required></input>
+                    <label for="password-field">Password</label>
+                    <input id="password-text" title="password" type="password" required></input>
+                    <div id="password-visibility-container">
+                        <label id="password-visibility-label" for="password-visibility">Toggle Password Visibility</label>
+                        <input 
+                            id="password-visibility" 
+                            title="password-visibility"
+                            type="checkbox"
+                            onClick={() => {
+                                let field = document.getElementById("password-text")
+                                field.type = field.type === "password" ? "text" : "password" 
+                            }}
+                            >
+                        </input>
+                    </div>
+                    <input id="login-button" type="submit" value="Login"></input>
                     <input 
-                        id="password-visibility" 
-                        title="password-visibility"
-                        type="checkbox"
+                        id="nav-button" 
+                        type="button" 
+                        value="Click here to create an account"
                         onClick={() => {
-                            let field = document.getElementById("password-text")
-                            field.type = field.type === "password" ? "text" : "password" 
+                            setState(0)
                         }}
                         >
                     </input>
-                </div>
-            <input id="signup-button" type="submit" value="Create account"></input>
-            </form>
+                </form>
+            );
+        } else {
+            return (
+                <form id="signup-form">
+                    <h1>Create your account</h1>
+                    <label class="required" for="first-name">First Name</label>
+                    <input title="first-name" type="text" required></input>
+                    <label class="required" for="last-name">Last Name</label>
+                    <input title="last-name" type="text" required></input>
+                    <label class="required" for="email">Email</label>
+                    <input title="email" type="email" required></input>
+                    <label class="required" for="password-field">Password</label>
+                    <input id="password-text" title="password" type="password" pattern="^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$" required></input>
+                    <div id="password-visibility-container">
+                        <label id="password-visibility-label" for="password-visibility">Toggle Password Visibility</label>
+                        <input 
+                            id="password-visibility" 
+                            title="password-visibility"
+                            type="checkbox"
+                            onClick={() => {
+                                let field = document.getElementById("password-text")
+                                field.type = field.type === "password" ? "text" : "password" 
+                            }}
+                            >
+                        </input>
+                    </div>
+                    <input id="signup-button" type="submit" value="Create account"></input>
+                    <input 
+                        id="nav-button" 
+                        type="button" 
+                        value="Click here to login"
+                        onClick={() => {
+                            setState(1)
+                        }}
+                        >
+                    </input>
+                </form>
+            );
+        }
+    }
+
+    return (
+        <div id="container">
+            {getPageContents(state)}
         </div>
     )
 }
