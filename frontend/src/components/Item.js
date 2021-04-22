462ce93f3bb05f55133882c733d5631b3226981b
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-04-22 18:27:14 +1000

    added styling for buttons to add items to cart on item page

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,6 +1,9 @@
 import { useHistory } from 'react-router-dom';
 import Review from './Review';
 
+import addshoppingcart96 from '../resources/addshoppingcart96.png';
+import '../styles/Item.css';
+
 const Item = () => {
 
     const history = useHistory();
@@ -9,11 +12,20 @@ const Item = () => {
     return (
         <div id="item-page-div">
             <div id="item-details-div">
-                <div>
+                <div id="item-image-div">
                     <img id="item-image" src={item.image} alt="The phone" />
-                    <button>Add to cart</button>
+                    <div id="item-actions-div">
+                        <div id="item-quantity-div">
+                            <button className="quantityButton" id="item-minus-button">-</button>
+                            <input id="item-quantity-field" type="text" pattern="[0-9]*" value="1" />
+                            <button className="quantityButton" id="item-plus-button">+</button>
+                        </div>
+                        <button id="item-add-cart-button">
+                            <img id="item-add-cart-icon" src={addshoppingcart96} alt="Add to checkout" />
+                        </button>
+                    </div>
                 </div>
-                <div>
+                <div id="item-text-div">
                     <p id="item-title">{item.title}</p>
                     <p id="item-brand">{item.brand}</p>
                     <p id="item-stock">{item.stock}</p>
