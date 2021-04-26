396f64c35be839511167e3795f67514aae387bdb
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-04-26 15:29:40 +1000

    implemented basic functionality to change quantity on item page

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -3,11 +3,29 @@ import Reviews from './Reviews';
 
 import addshoppingcart96 from '../resources/addshoppingcart96.png';
 import '../styles/Item.css';
+import { useRef } from 'react';
 
 const Item = () => {
 
     const history = useHistory();
     const item = history.location?.state;
+    const quantityInput = useRef(null);
+
+    const minusOnClickHandler = () => {
+        const curValue = parseInt(quantityInput.current.value);
+        if(curValue === 0) {
+            return
+        }
+        quantityInput.current.value = curValue - 1;
+    }
+
+    const plusOnClickHandler = () => {
+        const curValue = parseInt(quantityInput.current.value);
+        if(curValue === item.stock) {
+            return
+        }
+        quantityInput.current.value = curValue + 1;
+    }
 
     return (
         <div id="item-page-div">
@@ -16,9 +34,21 @@ const Item = () => {
                     <img id="item-image" src={item.image} alt="The phone" />
                     <div id="item-actions-div">
                         <div id="item-quantity-div">
-                            <button className="quantityButton" id="item-minus-button">-</button>
-                            <input id="item-quantity-field" type="text" pattern="[0-9]*" value="1" />
-                            <button className="quantityButton" id="item-plus-button">+</button>
+                            <button 
+                                className="quantityButton" 
+                                id="item-minus-button"
+                                onClick={minusOnClickHandler}>-</button>
+                            <input 
+                                id="item-quantity-field" 
+                                type="text" 
+                                pattern="[0-9]*" 
+                                value="0" 
+                                readOnly
+                                ref={quantityInput} />
+                            <button 
+                                className="quantityButton" 
+                                id="item-plus-button"
+                                onClick={plusOnClickHandler}>+</button>
                         </div>
                         <button id="item-add-cart-button">
                             <img id="item-add-cart-icon" src={addshoppingcart96} alt="Add to checkout" />
