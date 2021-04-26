bf357580cc78fde6d042ab42d78095c71a11263c
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-04-26 15:18:06 +1000

    implemented the show more button for long review comments

--- frontend/src/components/Review.js
+++ frontend/src/components/Review.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import Rating from './Rating';
 
 import '../styles/Review.css';
 
 const Review = ({ data }) => {
+
+    const longComment = data.comment.length > 200;
+    const [showLess, setShowLess] = useState(true);
+
+    const getComment = () => {
+        if(showLess && longComment) {
+            return `${data.comment.substring(0, 200)}...`
+        } else {
+            return data.comment;
+        }
+    }
+
+    const showMoreOnClick = () => {
+        setShowLess(!showLess);
+    }
 
     return (
         <div id="review-div">
@@ -10,7 +26,12 @@ const Review = ({ data }) => {
                 <p id="review-user">{`User: ${data.reviewer}`}</p>
                 <Rating rating={data.rating} />
             </div>
-            <p id="review-comment">{`${data.comment}`}</p>
+            <p id="review-comment">{getComment()}</p>
+            {longComment ?
+            <button id="review-show-more" 
+                onClick={showMoreOnClick}>
+                    {showLess ? "Show more" : "Show less"}
+            </button> : <></>}
         </div>
     )
 }
--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -1,9 +1,14 @@
 import Review from './Review';
 
+import '../styles/Reviews.css';
+
 const Reviews = ({ reviews }) => {
+
+    // TODO: Load 3 at a time
+
     return (
-        <div>
-            <p>Reviews</p>
+        <div id="reviews-div">
+            <h1 id="reviews-heading">Reviews</h1>
             <div>
                 {reviews.map(value => <Review data={value} />)}
             </div>
