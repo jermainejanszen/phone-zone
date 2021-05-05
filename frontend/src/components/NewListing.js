a46d1c939815e10a3f0494dd52c178a1fe31966f
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-05-05 12:40:00 +1000

    updated styling for new listing page

--- frontend/src/components/NewListing.js
+++ frontend/src/components/NewListing.js
@@ -1,11 +1,52 @@
 import React from 'react'
 import '../styles/Home.css';
-import OwnItem from './OwnItem';
+import Card from './Card';
 import '../styles/Profile.css';
 import '../styles/NewListing.css';
+import remove from '../resources/remove.svg';
+import hide96 from '../resources/hide96.png';
 
 const mockItems = [
     {
+        "title": "Galaxy s III mini SM-G730V Verizon Cell Phone BLUE",
+        "brand": "Samsung",
+        "image": "/phone_images/Samsung.jpeg",
+        "stock": 9,
+        "seller": "5f5237a4c1beb1523fa3db73",
+        "price": 56.0,
+        "reviews": [{
+            "reviewer": "5f5237a4c1beb1523fa3db1f",
+            "rating": 3,
+            "comment": "Got phone yesterday all ... pleased now!"
+        }]
+    },{
+        "title": "Sony Ericsson TM506 Unlocked QUAD-Band 3G GSM CellPhone",
+        "brand": "Sony",
+        "image": "/phone_images/Sony.jpeg",
+        "stock": 0,
+        "seller": "5f5237a4c1beb1523fa3da68",
+        "price": 173.0,
+        "reviews": [],
+        "disabled": ""
+    },{
+        "title": "Sony Ericsson TM506 Unlocked QUAD-Band 3G GSM CellPhone",
+        "brand": "Sony",
+        "image": "/phone_images/Sony.jpeg",
+        "stock": 0,
+        "seller": "5f5237a4c1beb1523fa3da68",
+        "price": 173.0,
+        "reviews": [],
+        "disabled": ""
+    },{
+        "title": "Sony Ericsson TM506 Unlocked QUAD-Band 3G GSM CellPhone",
+        "brand": "Sony",
+        "image": "/phone_images/Sony.jpeg",
+        "stock": 0,
+        "seller": "5f5237a4c1beb1523fa3da68",
+        "price": 173.0,
+        "reviews": [],
+        "disabled": ""
+    },{
         "title": "Galaxy s III mini SM-G730V Verizon Cell Phone BLUE",
         "brand": "Samsung",
         "image": "/phone_images/Samsung.jpeg",
@@ -51,18 +92,19 @@ const NewListing = () => {
     return (
         <div className="profileContainer">
             <div id="my-listings">
-            <h2>Your Listings</h2>
+                <h2>Your Listings</h2>
                 <div className="scrollMenu">
-                    <OwnItem className="ownItem" item={mockItems[0]} />
-                    <OwnItem className="ownItem" item={mockItems[1]} />
-                    <OwnItem className="ownItem" item={mockItems[0]} />
-                    <OwnItem className="ownItem" item={mockItems[1]} />
-                    <OwnItem className="ownItem" item={mockItems[1]} />
-                    <OwnItem className="ownItem" item={mockItems[0]} />
-                    <OwnItem className="ownItem" item={mockItems[1]} />
+                    {mockItems.map(item => {
+                        return (
+                            <div>
+                                <img className="itemIcon" src={hide96} alt="Hide Item" />
+                                <img className="itemIcon" src={remove} alt="Delete Item" />
+                                <Card className="ownItem" item={item} />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
-            <hr></hr>
             <div id="add-listing">
                 <form id="manage-listings-form">
                     <h2>Add a new item</h2>
