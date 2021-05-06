3e3da7295db695f58191421032aa56b59b3e356a
Jermaine Janszen <jjan3640@uni.sydney.edu.au>
2021-05-06 17:55:33 +1000

    fixed some bugs on the edit profile page

--- frontend/src/components/EditProfile.js
+++ frontend/src/components/EditProfile.js
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import '../styles/Profile.css';
 import '../styles/EditProfile.css';
 
 const Profile = () => {
+
     const history = useHistory();
+
+    const [firstName, setFirstName] = useState("Jane");
+    const [lastName, setLastName] = useState("Doe");
+    const [email, setEmail] = useState("jane@doe.com");
+
+    const firstNameInput = useRef(null);
+    const lastNameInput = useRef(null);
+    const emailInput = useRef(null);
+
     return (
         <div className="profileContainer">
-            <h1>Hi, Jane</h1>
+            <h1>{`Hi, ${firstName}`}</h1>
             <form id="profile-form"> 
                 <h2>Update your details:</h2>
                 <div className="fieldDiv">
-                    <label className="formLabel" for="firstName">First Name</label>
-                    <input className="formInputText" value="Jane" title="firstName" type="text" />
+                    <label className="formLabel" htmlFor="firstName">First Name</label>
+                    <input className="formInputText" value={firstName} title="firstName" type="text"
+                        ref={firstNameInput}
+                        onChange={() => setFirstName(firstNameInput.current?.value.substring(0, 12))} />
                 </div>
                 <div className="fieldDiv">
-                    <label className="formLabel" for="lastName">Last Name</label>
-                    <input className="formInputText" value="Doe" title="lastName" type="text" />
+                    <label className="formLabel" htmlFor="lastName">Last Name</label>
+                    <input className="formInputText" value={lastName} title="lastName" type="text"
+                        ref={lastNameInput}
+                        onChange={() => setLastName(lastNameInput.current?.value)} />
                 </div>
                 <div className="fieldDiv">
-                    <label className="formLabel" for="email">Email</label>
-                    <input className="formInputText" value="jane@doe.com" title="email" type="text" />
+                    <label className="formLabel" htmlFor="email">Email</label>
+                    <input className="formInputText" value={email} title="email" type="text"
+                        ref={emailInput}
+                        onChange={() => setEmail(emailInput.current?.value)} />
                 </div>
                 <button 
                     className="updateButton" 
                     onClick={() => {
-                        history.push('/editProfile');
+                        setFirstName(firstNameInput.current?.value);
+                        setLastName(lastNameInput.current?.value);
+                        setEmail(emailInput.current?.value);
                     }}>
                         Update Profile
                 </button>
