359bde6d84a97b060b2e5c7c6f9f533cf957ca91
hcra5146 <hcra5146@uni.sydney.edu.au>
2021-05-06 12:21:02 +1000

    sold out soon query

--- models/phoneData.js
+++ models/phoneData.js
@@ -7,32 +7,59 @@ var PhoneSchema = new mongoose.Schema(
          stock:Number,
          seller:String,
          price:Number,
-         reviews:Array,
-		 timestamp:String, 
-		 user:String, 
-		 anon:String},
+         reviews:[{
+          reviewer:String,
+          rating:Number,
+          comment:String
+         }
+         ]
+       }
 		 )
+
+// get all phones 
+PhoneSchema.statics.getPhones = function(callback) {
+    return this
+            .find({})
+            .exec(callback)
+}
+
+// Find five phone listings "sold out soon" which has more than 0 quantity and not disabled
+PhoneSchema.statics.soldOutSoon = function(callback) {
+    return this
+            .find({stock: {"$gt": 0}, disabled: {$exists: false} })
+            .sort({'stock':1})
+            .limit(5)
+            .exec(callback)
+}
+
+// Find best sellers
+PhoneSchema.statics.bestSellers = function(callback) {
+  return this
+          .find({ratings: {"$gt": 0}, disabled: {$exists: false} })
+          .sort({'stock':1})
+          .limit(5)
+          .exec(callback)
+}
 
 var Phone = mongoose.model('Phone', PhoneSchema, 'phone_data')
 
-Phone.getPhones = function() {
-    return new Promise((resolve, reject) => {
-        this.find((err, docs) => {
-            if (err) {
-                console.error(err)
-                return reject(err)
-            }
-            resolve (docs)
-        })
-    })
-}
+//call methods 
+// Phone.soldOutSoon(function(err,result){
+// 	if (err){
+// 		console.log("Query error!")
+// 	}else{
+// 		console.log(result)
+// 	}	
+// });
 
-Phone.getPhones()
-  .then(docs => {
-    console.log(docs)
-  })
-  .catch(err => {
-    console.error(err)
-  })
+// Phone.getPhones(function(err, result) {
+//   if (err){
+//     console.log("Query error!")
+//   } else {
+//     console.log(result)
+//   }
+// })
+  
 
 module.exports = Phone
+
