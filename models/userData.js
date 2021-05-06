8f5942dfabced001fb1b2c0eca5e5af0a3e2f1e6
hcra5146 <hcra5146@uni.sydney.edu.au>
2021-05-06 15:37:40 +1000

    update password query

--- models/userData.js
+++ models/userData.js
@@ -1,32 +1,70 @@
 var mongoose = require('./db')
+var ObjectId = require('mongoose').Schema.ObjectId
 
 var userSchema = new mongoose.Schema(
-		{firstname: String, 
+		{    _id: mongoose.SchemaTypes.ObjectId,
+         firstname: String, 
          lastname: String,
          email:String,
          password:String},
 		 )
 
-var User = mongoose.model('User', userSchema, 'user_data')
-
-User.getUsers = function() {
-    return new Promise((resolve, reject) => {
-        this.find((err, docs) => {
-            if (err) {
-                console.error(err)
-                return reject(err)
-            }
-            resolve (docs)
-        })
-    })
+// get all users
+userSchema.statics.getUsers = function(callback) {
+  return this
+          .find({})
+          .exec(callback)
 }
 
-User.getUsers()
-  .then(docs => {
-    console.log(docs)
-  })
-  .catch(err => {
-    console.error(err)
-  })
+// get users' password 
+userSchema.statics.getPassword = function(id, callback){
+  return this
+          .find({_id: id})
+          .select('password')
+          .exec(callback)
+}
+
+//update users' password  
+userSchema.statics.updatePassword = function(id, newPassword, callback){
+  return this
+          .update({_id: id}, {$set:{'password':newPassword}})
+          .exec(callback)
+        }
+
+
+
+var User = mongoose.model('User', userSchema, 'user_data')
+
+// User.getUsers(function(err, result) {
+//   if (err){
+//     console.log("Query error!")
+//   } else {
+//     console.log(result)
+//   }
+// })
+
+User.getPassword('5f5237a4c1beb1523fa3da65', function(err, result){
+  if (err){
+    console.log("Query error!")
+  } else {
+    console.log(result)
+  }
+})
+
+User.updatePassword('5f5237a4c1beb1523fa3da65', "potatosalad", function(err, result){
+  if (err){
+    console.log("Query error!")
+  } else {
+    console.log(result)
+  }
+})
+
+User.getPassword('5f5237a4c1beb1523fa3da65', function(err, result){
+  if (err){
+    console.log("Query error!")
+  } else {
+    console.log(result)
+  }
+})
 
 module.exports = User
