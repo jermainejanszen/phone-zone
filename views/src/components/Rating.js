79803255f8bfe1f9fde3e977362871fffd0db1bd
hcra5146 <hcra5146@uni.sydney.edu.au>
2021-05-24 13:16:28 +1000

    comments for Rating.js

--- views/src/components/Rating.js
+++ views/src/components/Rating.js
@@ -1,18 +1,22 @@
 import star96 from '../resources/star96.png';
 import starfilled96 from '../resources/starfilled96.png';
-
 import '../styles/Rating.css';
 
+/**
+ * renders ratings for items on the site 
+ */
 const Rating = ({ rating }) => {
 
     var stars = [];
     for (let i = 0; i < 5; i++) {
+        // if star is to be filled 
         if(i < rating) {
             stars.push(<img
                 key={i} 
                 src={starfilled96} 
                 alt="Filled star" 
                 className="ratingStar" />);
+        // if star is to be empty (has already filled stars up to given rating)
         } else {
             stars.push(<img
                 key={i} 
@@ -22,6 +26,7 @@ const Rating = ({ rating }) => {
         }
     }
 
+    // handles case where number is NaN 
     if (Number.isNaN(rating)) {
         return <p>No ratings.</p>
     }
